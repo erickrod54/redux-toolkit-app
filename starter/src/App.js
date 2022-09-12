5044811fa272c8aef8bc18f833858de47343141e
Erick Rodriguez <erickrod54@gmail.com>
2022-09-12 02:25:47 -0400

    React-redux toolkit app - version 2 - App js Features: Importing and placing CartContainer Component in order to display it

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,8 +1,23 @@
 import Navbar from "./components/Nabvar";
+import CartContainer from "./components/CartContainer";
+
+
+/**React-redux toolkit app - version 2  - App js js - 
+ * Features:
+ * 
+ *      --> Importing and placing 'CartContainer' Component
+ *          in order to display it.
+ * 
+ * Note: Setting amount value will display the CartContainer
+ * reference to CarContainer code.
+ */
 
 function App() {
   return(
-    <Navbar />
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
   );
 }
 export default App;
