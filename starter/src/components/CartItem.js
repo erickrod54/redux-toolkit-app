eea7b3cfa926b28b9b1366fce37898b724c1ca37
Erick Rodriguez <erickrod54@gmail.com>
2022-09-12 02:33:08 -0400

    React-reduxx tool - app version 2 - Features: Building basic CartItem Component

---
+++ starter/src/components/CartItem.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+/**React-redux toolkit app - version 2  - 'CartItem' js - 
+ * Features:
+ * 
+ *      --> Building basic 'CartItem' Component.
+ * 
+ * Note: Setting amount value will display the CartContainer
+ * reference to CarContainer code.
+ */
+
+const CartItem = () => {
+    return(
+        <div>CartItems</div>
+    )
+}
+
+export default CartItem;
