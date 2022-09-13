4dd79d48e41cb4158ff8352fa549f16d8541a237
Erick Rodriguez <erickrod54@gmail.com>
2022-09-13 01:55:27 -0400

    React-redux toolkit app - version 3 - CartContainer - Features: Implementing useDispatch hook from react-redux to dispatch clearCart action, Triggering clearCart in dispatch to clear the items from the cart

--- starter/src/components/CartContainer.js
+++ starter/src/components/CartContainer.js
@@ -1,25 +1,29 @@
 import React from "react";
 import CartItem from "./CartItem";
-import { useSelector } from 'react-redux'
+import { useDispatch ,useSelector } from 'react-redux';
+import { clearCart } from '../features/cart/cartSlice'
 
-/**React-redux toolkit app - version 2  - 'CartContainer' js - 
+
+/**React-redux toolkit app - version 3  - 'CartContainer' js - 
  * Features:
  * 
- *      --> Destructuring 'cartItems', 'total', 'amount' 
- *          from useSelector
+ *      --> Implementing 'useDispatch' hook from
+ *          'react-redux' to dispatch 'clearCart'
+ *          action.
  * 
- *      --> Depending on the 'amount', rendering 
- *          conditionally an empty message or 
- *          the comtainer.
+ *      --> Triggering 'clearcart' in dispacth to 
+ *          clear the items from the cart
  * 
- *      --> Mapping the 'cartItems' data and spreading
- *          props 'CartItem' Component
- * 
- * Note: Setting amount value will display the CartContainer
- * reference to CarContainer code.
+ * Note: i define the functuionality in the slice
+ * 'cartSlice' and invoke it in dispatch action
+ * and will trigger 'clearCart'.
  */
 
 const CartContainer = () => {
+
+    /**here i keep the hook in a varibale */
+    const dispatch = useDispatch()
+
     const {cartItems, total, amount }  = useSelector(( store ) => store.cart)
 
     console.log(cartItems)
@@ -51,7 +55,14 @@ const CartContainer = () => {
                     total <span>${total}</span>
                 </h4>
             </div>
-            <button className="btn clear-btn">clear cart</button>
+            {/**here on click -using arrow function- 
+             * i implement the hook to invoke 'clearCart'
+             * action.
+            */}
+            <button 
+                className="btn clear-btn" 
+                onClick={ () => dispatch(clearCart())}
+                >clear cart</button>
         </footer>
     </div>
 }
