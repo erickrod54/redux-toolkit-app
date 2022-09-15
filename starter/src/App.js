9c8b0dd4873b0bb67cd1afa4d15ff394dac198aa
Erick Rodriguez <erickrod54@gmail.com>
2022-09-15 01:16:32 -0400

    React-redux toolkit app - version 6 - App js - Features: Destructuring cartItems from the store, Dispatching calculateTotals to render totals when the app mounts

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,18 +1,36 @@
 import Navbar from "./components/Nabvar";
+import { useEffect } from "react";
 import CartContainer from "./components/CartContainer";
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotals } from "./features/cart/cartSlice";
 
-
-/**React-redux toolkit app - version 2  - App js js - 
+/**React-redux toolkit app - version 6  - App js js - 
  * Features:
  * 
- *      --> Importing and placing 'CartContainer' Component
- *          in order to display it.
+ *      --> Destructuring 'cartItems' from the 'store'.
+ * 
+ *      --> Dispatching 'calculateTotals' render the 
+ *          totals when the app mounts.
  * 
  * Note: Setting amount value will display the CartContainer
  * reference to CarContainer code.
  */
 
 function App() {
+
+  /**here i destructure cartItems */
+  const { cartItems } = useSelector((store) => store.cart)
+
+  /**here i assign the 'dispatch' hook */
+  const dispatch = useDispatch() 
+
+  /**here i trigger the 'calculateTotals' depending on
+   * the 'cartItems' existence */
+  useEffect(() =>{
+    dispatch(calculateTotals())
+    // eslint-disable-next-line
+  },[cartItems]);
+
   return(
     <main>
       <Navbar />
