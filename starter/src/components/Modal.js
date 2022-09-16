99c32c495c41b831e8c007cc6b2b3694cf3309fc
Erick Rodriguez <erickrod54@gmail.com>
2022-09-16 00:32:47 -0400

    React-redux toolkit app - version 8 - Modal js - Features: Importing and dispatching clearCart and closeModal on confirm button, Importing and dispatching closeModal on cancel button

--- starter/src/components/Modal.js
+++ starter/src/components/Modal.js
@@ -1,25 +1,51 @@
 import React from "react";
+import { clearCart } from "../features/cart/cartSlice";
+import { closeModal } from "../features/modal/modalSlice";
+import { useDispatch } from "react-redux";
 
-/**React-redux toolkit app - version 7  - Modal js - 
+/**React-redux toolkit app - version 8  - Modal js - 
  * Features:
  * 
- *      --> Building the 'Modal' js Component
+ *      --> Importing and dispatching 'clearCart()' and 
+ *          'closeModal()' on confirm button.
  * 
- * Note: this component will render a modal with two
- * options to confirm to clear the data
+ *      --> Importing and dispatching 'closeModal()' on 
+ *          cancel button.
+ * 
+ * Note: this last functionality implemented will replaced
+ * the 'clearCart' directlly and will add a confirmation
+ * step.
+ * 
  */
 
 const Modal = () => {
+
+    /**here i asign the 'dispatch' hook to dispatch 
+     * variable */
+    const dispatch = useDispatch()
 
     return(
         <aside className="modal-container">
             <div className="modal">
                 <h4>remove all items from the cart ?</h4>
                 <div className="btn-container">
-                    <button type='button' className="btn confirm-btn">
+                    <button 
+                        type='button' 
+                        className="btn confirm-btn"
+                        onClick={() => {
+                            dispatch(clearCart())
+                            dispatch(closeModal())
+                        }}
+                        >
                         confirm
                     </button>
-                    <button type='button' className="btn clear-btn">
+                    <button 
+                        type='button' 
+                        className="btn clear-btn"
+                        onClick={() => {
+                            dispatch(closeModal())
+                        }}
+                        >
                         cancel
                     </button>
                 </div>
