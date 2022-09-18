c07927c3cc7395588c68bd121099dd1a50e89300
Erick Rodriguez <erickrod54@gmail.com>
2022-09-18 11:48:46 -0400

    React-redux toolkit app - version 10 - App js - Features: Testing axios options combined with asyncThunk - passing a string value throught the async-useEffect trigger-

--- starter/src/App.js
+++ starter/src/App.js
@@ -5,40 +5,38 @@ import { useDispatch, useSelector } from 'react-redux';
 import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
 import Modal from "./components/Modal";
 
-/**React-redux toolkit app - version 9  - App js js - 
+/**React-redux toolkit app - version 10  - App js js - 
  * Features:
  * 
- *      --> Destructuring 'isLoading' state from the 
- *          'store.cart'.
- * 
- *      --> Building a second 'useEffect' in order to
- *          dispatch the 'getCartItems' actions once 
- *          the 'App' mounts
+ *      --> Testing 'axios' options combined with 
+ *          'asyncThrunk'
  * 
  * Note: I can check how the actions get done in detail 
  * using 'DevTools' > 'Redux' tab
  * 
+ * The testing value that im passing throught the 'getCartItems'
+ * > 'random' is being passed when the component mounts ( in
+ * this case but can be made when the useEffect triggers for
+ * some reason that i need )
  */
 
 function App() {
 
   
   const { cartItems, isLoading } = useSelector((store) => store.cart)
-
-  /**here i destructure 'isOpen' from the store*/
   const { isOpen } = useSelector((store) => store.modal)
   
   const dispatch = useDispatch() 
 
-  /**here i trigger the 'calculateTotals' depending on
-   * the 'cartItems' existence */
   useEffect(() =>{
     dispatch(calculateTotals())
     // eslint-disable-next-line
   },[cartItems]);
 
+  /**using axios i can pass values throught the
+   * async and get them direclly in the 'getCartItems'*/
   useEffect(() => {
-    dispatch(getCartItems())
+    dispatch(getCartItems('random'))
     // eslint-disable-next-line
   }, [])
 
